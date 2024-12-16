2b5bc6b03a6bd5cf609af64d6493addb4454e562
Ilan Manzari <ilanmanzari2607@gmail.com>
2024-12-16 16:36:37 +0100

    Commit #7 : Tableau de 4 carrés initialisés

--- mcarre.cpp
+++ mcarre.cpp
@@ -8,13 +8,13 @@ using namespace std;
 
 int main()
 {
-	CCarre carre;
+	/*CCarre carre;
 	carre.Setsx(2);
 	carre.Setsy(4);
 	carre.Setcote(8);
 	carre.Afficher();
 
-	/*cout << endl;
+	cout << endl;
 	cout << "Test des getters :" << endl;
 	cout << "Coordonnee sx (via Getsx) : " << carre.Getsx() << endl;
 	cout << "Coordonnee sy (via Getsy) : " << carre.Getsy() << endl;
@@ -24,7 +24,7 @@ int main()
 
 	// Affichage après déplacement
 	cout << "Caracteristiques apres deplacement(direction et saut) :" << endl;
-	carre.Afficher();*/
+	carre.Afficher();
 
 	// Test de la surcharge Deplacer (vecteur dx, dy)
 	int dx, dy;
@@ -37,7 +37,31 @@ int main()
 	carre.Deplacer(dx, dy);
 
 	cout << "\nCaracteristiques apres deplacement (vecteur dx, dy) :" << endl;
-	carre.Afficher();
+	carre.Afficher();*/
 
+	// Création d'un tableau de 4 carrés
+	CCarre carres[4];
 
+	// Initialisation des carrés avec des valeurs différentes
+	carres[0].Setsx(5);
+	carres[0].Setsy(4);
+	carres[0].Setcote(10);
+	carres[1].Setsx(2);
+	carres[1].Setsy(3);
+	carres[1].Setcote(20);
+	carres[2].Setsx(1);
+	carres[2].Setsy(6);
+	carres[2].Setcote(25);
+	carres[3].Setsx(7);
+	carres[3].Setsy(8);
+	carres[3].Setcote(30);
+	
+	// Affichage des caractéristiques de chaque carré
+	cout << "Tableau de carres :" << endl;
+	for (int i = 0; i < 4; ++i)
+	{
+		cout << "Carre " << i + 1 << ":" << endl;
+		carres[i].Afficher();
+		cout << endl;
+	}
 }
